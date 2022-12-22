0bc233dddf16e5e5b9ab2b301b109c12d9e8740c
Yukiniro <yukiniro@hotmail.com>
2022-12-22 22:02:33 +0800

    feat: add unocss

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,19 @@
 import { useState } from "react";
-import Style from "./App.module.less";
 import { ReactComponent as LogoSvg } from "./icon/logo.svg";
 import { ReactComponent as FaviconSvg } from "./icon/favicon.svg";
 
 function App() {
   const [count, setCount] = useState(0);
   return (
-    <div className={Style.app}>
-      <h1 className={Style.title}>Thank to use vite-react-template</h1>
-      <div>
-        <LogoSvg />
-        <FaviconSvg />
+    <div className="w-screen h-screen bg-gray-800 text-center flex flex-col items-center justify-center">
+      <h1 className="text-white text-20 font-mono font-600">Hello World</h1>
+      <div className="flex p-14 w-110 justify-between items-center">
+        <LogoSvg className="w-60 h-60" />
+        <FaviconSvg className="w-40 h-40" />
       </div>
       <p>
         <button
-          type="button"
-          className={Style.button}
+          className="cursor-pointer text-6 px-6 py-2 rounded-4 border-none"
           onClick={() => setCount(count => count + 1)}
         >
           count is: {count}
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import Unocss from "unocss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react(), svgr(), Unocss()],
 });
